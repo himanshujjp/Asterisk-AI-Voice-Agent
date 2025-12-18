99c86386dace6f57961ad5609a054378ae47b712
Haider Jarral <haider.jarral@illumio.com>
2025-12-17 20:05:51 -0800

    debug: log ALL unhandled ElevenLabs message types to catch tool calls

--- src/providers/elevenlabs_agent.py
+++ src/providers/elevenlabs_agent.py
@@ -446,7 +446,8 @@ class ElevenLabsAgentProvider(AIProviderInterface, ProviderCapabilitiesMixin):
             await self._handle_error(data)
         
         else:
-            logger.debug(f"[elevenlabs] [{self._call_id}] Unhandled message type: {msg_type}")
+            # Log ALL unhandled message types at info level to catch tool calls
+            logger.info(f"[elevenlabs] [{self._call_id}] Unhandled message type: {msg_type}, keys: {list(data.keys())}")
     
     async def _handle_conversation_init(self, data: Dict[str, Any]) -> None:
         """Handle conversation initialization metadata."""
